42af679aa6c787a4400a2c8e49531008f8ee281f
Taylor Bolt <taylor_bolt@Taylors-Air.lan>
2020-11-25 15:20:13 -0500

    Updates to preprocessing

--- preprocess_rest.sh
+++ preprocess_rest.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 # Make directories
-mkdir -p data/rest/raw
 mkdir -p data/rest/proc_1_smooth
 mkdir -p data/rest/proc_2_norm_filter
 mkdir -p data/rest/proc_3_surf_resamp
 mkdir -p data/rest/proc_4_gs_regress
-mkdir -p data/rest/proc_2_surf_resamp
 
 # Smooth 
-echo "smoothing..." 
-./preprocess/surface_smooth.sh data/rest/raw data/rest/proc_1_smooth
-# Normalize (zscore) and filter
-echo "norming and filtering..."
-preprocess/norm_filter.sh data/rest/proc_1_smooth data/rest/proc_2_norm_filter
-# Resample to fsaverage4
-echo "resampling..."
-preprocess/fsaverage_resample.sh data/rest/proc_2_norm_filter data/rest/proc_3_surf_resamp
+# echo "smoothing..." 
+# ./preprocess/surface_smooth.sh data/rest/raw data/rest/proc_1_smooth
+# # Normalize (zscore) and filter
+# echo "norming and filtering..."
+# preprocess/norm_filter.sh data/rest/proc_1_smooth data/rest/proc_2_norm_filter
+# # Resample to fsaverage4
+# echo "resampling..."
+# preprocess/fsaverage_resample.sh data/rest/proc_2_norm_filter data/rest/proc_3_surf_resamp
 # Remove global signal
 echo "removing global signal..."
 preprocess/global_signal_regress.sh data/rest/proc_3_surf_resamp data/rest/proc_4_gs_regress
 
 
-# Resample smoothed data to fsaverage4 - for wavelet decomposition 
-echo "resampling...again..."
-preprocess/fsaverage_resample.sh data/rest/proc_1_smooth data/rest/proc_2_surf_resamp
